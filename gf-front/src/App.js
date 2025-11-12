df2343d79732741959da68398f314779516617bc
Gustavo <gus.carraro@hotmail.com>
2025-11-11 21:27:08 -0300

    ajuste1

---
+++ gf-front/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GlobalStyle } from './styles/global';
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Gestão Financeira ??</h1>
+    </>
+  );
+}
---
+++ gf-front/src/styles/global.js
@@ -0,0 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * { margin: 0; padding: 0; box-sizing: border-box; }
+  body { font-family: "Roboto", sans-serif; background: #f4f6f8; color: #333; }
+`;
